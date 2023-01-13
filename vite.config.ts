a43fea620f84a7e949acd7b1d42a1c114e06b29a
Jacobo <22jacganon@gmail.com>
2023-01-13 02:06:00 -0700

    Inserted UI Framework with forced dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import suidPlugin from "@suid/vite-plugin";
+
+
   
 export default defineConfig(() => {
   return {
-    plugins: [solid({ ssr: false })],
+    plugins: [solid({ ssr: false }), suidPlugin(), solidPlugin()],
+    build: {
+      target: "esnext",
+    }
   };
 });
-  
